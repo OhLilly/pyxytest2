40b887af1adf0d471e6f9c74a08b304dc9e8d2b6
Lina El Yakhloufi <lina.elyakhloufi@pyxo.fr>
2022-06-13 16:07:10 +0000

    commit-report-nullvaluesproductIds

--- models/reporting/products_paired_status.sql
+++ models/reporting/products_paired_status.sql
@@ -1,20 +1,18 @@
 select
-    dc.deposit_id as identifiant_deposit,
+    pp.deposit_id as identifiant_deposit,
     pp.product_id as identifiant_produit,
     pp.product_name as nom_produit,
-    dc.sent_at as date_appairage,
+    pp.sent_at as date_appairage,
     pr.sent_at as date_retour,
-    dc.location as lieu_appairage,
-    dc.user_id as identifiant_utilisateur,
+    pp.location as lieu_appairage,
+    pp.user_id as identifiant_utilisateur,
     CASE
-WHEN getdate()<= dateadd(day,6,dc.sent_at) and date_retour is null then 'appairé'
-WHEN getdate()> dateadd(day,6,dc.sent_at) and date_retour is null then 'expired'
+WHEN getdate()<= dateadd(day,6,pp.sent_at) and date_retour is null then 'appairé'
+WHEN getdate()> dateadd(day,6,pp.sent_at) and date_retour is null then 'expired'
 ELSE 'retourné'
 END AS statuts
-from {{ ref('deposit_created') }} as dc
-left join {{ ref('products_paired') }} as pp
-on dc.deposit_id=pp.deposit_id
+from {{ ref('products_paired') }} as pp
 left join {{ ref('products_returned') }} as pr
-on dc.deposit_id=pr.deposit_id
+on pp.deposit_id=pr.deposit_id
 order by date_appairage desc
 

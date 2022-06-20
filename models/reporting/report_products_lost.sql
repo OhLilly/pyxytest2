510d91185c822f6f37a230044830f68a6cfc867e
Romaric Haldenwang <romaric@pyxo.fr>
2022-06-20 08:04:19 +0000

    commit-noprimarykey

--- models/reporting/report_products_lost.sql
+++ models/reporting/report_products_lost.sql
@@ -1,16 +1,16 @@
 select
-    deposit_id as identifiant_deposit,
-    product_id as identifiant_produit,
-    product_name as nom_produit,
-    max(sent_at) as derniere_date_appairage,
-    user_id as identifiant_utilisateur,
-    location as lieu_dernier_appairage
+    deposit_id, --as identifiant_deposit
+    product_id, --as identifiant_produit
+    product_name, --as nom_produit
+    max(sent_at), --as derniere_date_appairage
+    user_id, --as identifiant_utilisateur
+    location --as lieu_dernier_appairage
 from {{ ref('stg_products_paired') }}
 where deposit_id in 
 (select deposit_id from {{ ref('stg_deposit_expired') }}) AND (deposit_id not in (select deposit_id from {{ ref('stg_products_returned') }}))
 group by
-    identifiant_deposit,
-    identifiant_produit,
-    nom_produit,
-    identifiant_utilisateur,
-    lieu_dernier_appairage
+  deposit_id,
+  product_id,
+  product_name,
+  user_id,
+  location
--- models/staging/stg_deposit_created.sql
+++ models/staging/stg_deposit_created.sql
@@ -2,9 +2,9 @@ select
 dc.deposit_id,
 dc.user_id,
 dc.location,
-dp.products,
+--dp.products,
 dc.sent_at
 from {{ ref('raw_dev_prodbackend_depositcreated') }} as dc
-left join {{ ref('raw_dev_prodbackendcreated_depositpaired') }} as dp
-on dc.deposit_id=dp.deposit_id
+--left join {{ ref('raw_dev_prodbackendcreated_depositpaired') }} as dp
+--on dc.deposit_id=dp.deposit_id
 order by dc.timestamp desc
